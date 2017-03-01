40cbd5104bfc902892983e4b45047e16050dc5a8
David Schneider <davidsch@slac.stanford.edu>
2017-03-01 15:19:33 -0800

    question about vds_unlimited - code I setn to Jialin

---
+++ questions/vds_unlimited/driver.py
@@ -0,0 +1,45 @@
+import h5py
+import numpy as np
+import os
+import sys
+import time
+
+def run_command(command):
+    t0 = time.time()
+    print("-----------")
+    print(command)
+    res = os.system(command)
+    print(" -- finished in %.2f sec" % (time.time()-t0,))
+    assert res == 0, "command failed"
+
+if __name__ == '__main__':
+    assert len(sys.argv)==2, "usage: driver.py N where N is how many elements in src dsets"
+    N = int(sys.argv[1])
+
+    t0 = time.time()
+    ones = np.ones(N).astype(np.int16)
+    twos = 2*np.ones(N).astype(np.int16)
+
+    srcA = h5py.File('srcA.h5','w')
+    srcA['dsetA'] = ones
+    srcA.close()
+
+    srcB = h5py.File('srcB.h5','w')
+    srcB['dsetB'] = twos
+    srcB.close()
+    print("created files in %.2f sec" % (time.time()-t0,))
+    run_command('h5c++ vds_unlimited.cpp -o vds_unlimited')
+    run_command('./vds_unlimited %d' % N)
+
+    t0 = time.time()
+    master = h5py.File('master.h5','r')
+    vds_data = master['vds'][:]
+
+#    np.testing.assert_equal(vds_data[0::2], ones)
+#    np.testing.assert_equal(vds_data[1::2], twos)
+    print("read vds in %.2f sec" % (time.time()-t0,))
+    print("QUESTION: Why is the vds file so big? ")
+    print("QUESTION: Why does h5py take so long to read back the data?")
+    sys.stdout.flush()
+    os.system('ls -lrth')
+
---
+++ questions/vds_unlimited/vds_unlimited.cpp
@@ -0,0 +1,58 @@
+#include <cstdio>
+#include <cstdlib>
+#include "hdf5.h"
+
+/*
+  Before running this, create two files
+
+  srcA.h5
+  srcB.h5
+
+  which have the datasets:
+  srcA.h5/dsetA  N native short
+  srcB.h5/dsetB  N native short
+
+  where N is the datasets lengths, and it is passed into this routine
+ */
+int main(int argc, char *argv[]) {
+  if (argc != 2) {
+    fprintf(stderr, "usage: vds_fixed N where N is how many elements in src dsets\n");
+    return -1;
+  }
+
+  hsize_t sizeN = atol(argv[1]);
+  hsize_t size2N=2*sizeN;
+
+  hid_t fapl = H5Pcreate(H5P_FILE_ACCESS);
+  H5Pset_libver_bounds(fapl, H5F_LIBVER_LATEST, H5F_LIBVER_LATEST);
+  hid_t fid = H5Fcreate("master.h5", H5F_ACC_TRUNC, H5P_DEFAULT, fapl);
+
+  short fill = -1;
+  hid_t dcpl = H5Pcreate(H5P_DATASET_CREATE);
+  H5Pset_fill_value(dcpl, H5T_NATIVE_SHORT, &fill);
+ 
+  const int rank1 = 1;
+  hsize_t size0=0;
+  hsize_t unlimited = H5S_UNLIMITED;
+  hid_t vds_space = H5Screate_simple(rank1, &size0, &unlimited);
+  hid_t src_space = H5Screate_simple(rank1, &size0, &unlimited);
+  
+  hsize_t start0=0, start1=1, stride2=2, block1=1;
+  
+  H5Sselect_hyperslab(vds_space, H5S_SELECT_SET, &start0, &stride2, &unlimited, &block1);
+  H5Pset_virtual(dcpl, vds_space, "srcA.h5", "/dsetA", src_space);
+  
+  H5Sselect_hyperslab(vds_space, H5S_SELECT_SET, &start1, &stride2, &unlimited, &block1);
+  H5Pset_virtual(dcpl, vds_space, "srcB.h5", "/dsetB", src_space);
+  
+  hid_t dset = H5Dcreate2(fid, "vds", H5T_NATIVE_SHORT, vds_space, 
+                          H5P_DEFAULT, dcpl, H5P_DEFAULT);
+  H5Dclose(dset);
+  H5Sclose(src_space);
+  H5Sclose(vds_space);
+  H5Pclose(dcpl);
+  H5Fclose(fid);
+  H5Pclose(fapl);
+  return 0;
+}
+
