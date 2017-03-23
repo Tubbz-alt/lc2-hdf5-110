0b2ce3447a1af73053e0f2e9b3064a28a9297b21
David Schneider <davidsch@slac.stanford.edu>
2017-03-23 14:13:32 -0700

    update

--- src/DsetInfo.cpp
+++ src/DsetInfo.cpp
@@ -46,8 +46,8 @@ DsetReaderInfo &DsetReaderInfo::operator=( DsetReaderInfo &o) {
 void DsetReaderInfo::dim(const std::vector<hsize_t> &new_dim) {
   if (dset_id() < 0) throw std::runtime_error("DsetReaderInfo::dim - invalid dset_id");
   if (file_space_id() < 0) throw std::runtime_error("DsetReaderInfo::dim - invalid file_space_id");
-  m_dim = dim;
-  NONNEG( H5set_extent_simple( m_file_space_id, m_dim.size(), &m_dim.at(0), NULL) );
+  DsetInfo::dim(dim);
+  NONNEG( H5set_extent_simple( m_file_space_id, dim().size(), dim().at(0), NULL) );
 }
 
 void DsetReaderInfo::select_file_space_id(int64_t event_index) {
